2d75ff9b6c19d02fcc08b3320b2cdb1ac52bd217
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-10 21:03:49 +0700

    useReducer Hook

---
+++ 10-usereducer-hook/src/Components/Counter.tsx
@@ -0,0 +1,44 @@
+import { useReducer } from "react";
+
+type CounterState = {
+  count: number;
+};
+
+type ActionTypes = {
+  type: string;
+  payload: number;
+};
+
+const initialState = { count: 0 };
+
+const reducer = (state: CounterState, action: ActionTypes) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + action.payload };
+
+    case "decrement":
+      return { count: state.count - action.payload };
+
+    default:
+      return state;
+      break;
+  }
+};
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      <h1>Counter</h1>
+      <p>{state.count}</p>
+      <button onClick={() => dispatch({ type: "increment", payload: 10 })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
+        Decrement
+      </button>
+    </>
+  );
+};
+
+export default Counter;
